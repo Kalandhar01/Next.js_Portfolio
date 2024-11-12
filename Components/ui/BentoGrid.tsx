bd011738186a91f87d1f8f410b9522f3e0a8bf28
KALANDHAR <kalandars2004@gmail.com>
2024-11-12 07:08:48 +0530

    Tdy done

--- Components/ui/BentoGrid.tsx
+++ Components/ui/BentoGrid.tsx
@@ -9,7 +9,7 @@ export const BentoGrid = ({
   children?: React.ReactNode;
 }) => {
   return (
-    <div
+    <div  
       className={cn(
         "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
         className
@@ -81,7 +81,9 @@ export const BentoGridItem = ({
 
 { id===6 && (
   <BackgroundGradientAnimation>
-    <div className="absolute z-50 flex items-center justify-center text-white font-bold  "></div>
+    <div className="absolute z-50 flex items-center justify-center text-white font-bold  ">
+
+    </div>
 
   </BackgroundGradientAnimation>
 
@@ -89,23 +91,35 @@ export const BentoGridItem = ({
 
 
 }
+
+<div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')} >
+<div className="font-sans font-extralight  text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+  {description}
+
+</div>
+<div className={cn("font-sans font-bold text-lg lg:text-base z-10", titleClassName)}>
+          {title}
+        </div>
+
+</div>
         
 
       </div>
 
+      {
+        id ===2 &&
+        
+        <div>
 
-
-
-
-      {/* Image or other content can be added here */}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className={cn("font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2", titleClassName)}>
-          {title}
+          {/* <Globe/> */}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
-      </div>
+      }
+
+
+
+
+
+      
     </div>
   );
 };
