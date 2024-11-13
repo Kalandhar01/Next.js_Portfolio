3fc13a14c1107ba0510db21d3f3a2b53d62f237b
KALANDHAR <kalandars2004@gmail.com>
2024-11-13 14:25:29 +0530

    brefore deploy

---
+++ Components/Dummy.tsx
@@ -0,0 +1,15 @@
+import { CanvasRevealEffectDemo } from './CanvasRevealEffectDemo'
+
+const Dummy = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <CanvasRevealEffectDemo >
+
+        </CanvasRevealEffectDemo>
+        
+      
+    </div>
+  )
+}
+
+export default Dummy
